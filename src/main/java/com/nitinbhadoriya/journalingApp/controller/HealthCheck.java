5daabb37933fae8aa9290f5af7aa9a142f8e6322
Nitin Bhadoriya <nitinbhadoriya@Nitins-MacBook-Air.local>
2025-01-25 20:50:45 +0530

    Creating project outline

---
+++ src/main/java/com/nitinbhadoriya/journalingApp/controller/HealthCheck.java
@@ -0,0 +1,15 @@
+package com.nitinbhadoriya.journalingApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status")
+public class HealthCheck {
+
+    @GetMapping
+    public String healthCheck() {
+        return "OK";
+    }
+}
